e79ebb34ccb513f8021b787ced5c7e8777dcfcbe
Tola Veng <tolaswin@gmail.com>
2020-05-09 17:23:22 +1000

    tola: display meter data

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import MeterDataView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='webapp-home'),
+    path('meterdata', MeterDataView.as_view(), name='meterdata')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+from .models import MeterData
 
 def home(request):
     return render(request, 'webapp/home.html')
+
+class MeterDataView(ListView):
+    title = 'MeterData'
+    model = MeterData
+    template_name = 'webapp/meterdata.html'
+    context_object_name = 'meterdata'
+    ordering = ['-ReadAt']
+    paginate_by = 10
+    #paginated_orphans = 50
+    #queryset = MeterData.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(MeterDataView, self).get_context_data(**kwargs)
+        filter_set = MeterData.objects.all()
+
+        #current_page = context.pop('page_obj', None)
+        #context['current_page'] = current_page
+        #context['ReadAt']
+        return context
