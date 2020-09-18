62e77a06e2b6c98b1ef064120d4a41faffba8211
Michael Tonini <michael.jack.tonini@gmail.com>
2020-09-18 17:51:58 +1000

    changes from live testing

--- Prediction/create_table_and_insert_data.py
+++ Prediction/create_table_and_insert_data.py
@@ -1,150 +1,93 @@
 import sys
-import csv
 import mysql.connector
-
+import pandas as pd
+import sqlalchemy
 
 # --------- Database Configuration ---------
-DB_HOST = 'localhost'
+DB_HOST = 'web-db.c6vnael0nhls.ap-southeast-2.rds.amazonaws.com'
 DB_PORT = 3306
-DB_NAME = 'meterdb-dev'
-DB_USER = 'root'
-DB_PASSWORD = 'root'
+DB_NAME = 'main_DB'
+DB_USER = 'detectivePretzel'
+DB_PASSWORD = 'brunoDiplomat'
 
 # --------- End Database Configuration ---------
 
+""""
+########### Read From File For Testing ###########
+predicted_data = pd.read_csv("predicteddata.csv", infer_datetime_format=True, index_col=['FutureDateMonth'])
+predicted_data['KWH'] = pd.to_numeric(predicted_data['KWH'])
+predicted_data['MeterID'] = 98801006
+########### Read From File For Testing ###########
+"""
 
-def Database_Connection(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD):
+
+def database_connection_with_sqlalchemy():
     try:
-        mydb = mysql.connector.connect(
-            host=DB_HOST,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-    except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
+        mydb_sqlalchemy = sqlalchemy.create_engine(
+            'mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}'.format(
+                DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME),
+            echo=False)
+        print("Succeed sqlalchemy")
+        return mydb_sqlalchemy
+    except Exception as ex:
+        template_message = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template_message.format(type(ex).__name__, ex.args)
+        print(message)
         sys.exit()
-    return mydb
 
 
-mydb = Database_Connection(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD)
-print(mydb)
-print("Connect Succeed")
-
-########################################
-#Check table not exist, then create
-mycursor = mydb.cursor()
-sql_check_table_ServiceArea_not_exist_create = "CREATE TABLE IF NOT EXISTS ServiceArea (AreaID INT PRIMARY KEY, AreaName VARCHAR(255))"
-mycursor.execute(sql_check_table_ServiceArea_not_exist_create)
-
-sql_create_Meter = "CREATE TABLE IF NOT EXISTS Meter (MeterID INT UNIQUE, AreaID INT, FOREIGN KEY (AreaID) REFERENCES ServiceArea(AreaID))"
-mycursor.execute(sql_create_Meter)
-
-sql_check_table_MeterData_not_exist_create = """CREATE TABLE IF NOT EXISTS MeterData (
-                                                ID INT AUTO_INCREMENT PRIMARY KEY,
-                                                MeterID INT,
-                                                ReadAt DATETIME,
-                                                KWH FLOAT,
-                                                KW FLOAT,
-                                                KVA FLOAT,
-                                                KVAr FLOAT,
-                                                Ph1i FLOAT,
-                                                Ph2i FLOAT,
-                                                Ph3i FLOAT,
-                                                Ph1v FLOAT,
-                                                Ph2v FLOAT,
-                                                Ph3v FLOAT,
-                                                PF FLOAT,
-                                                FOREIGN KEY (MeterID) REFERENCES Meter(MeterID)
-                                                )"""
-mycursor.execute(sql_check_table_MeterData_not_exist_create)
-
-
-####################################
-#For Global Active Power
-sql_global_active_power = """CREATE TABLE IF NOT EXISTS Household_power (
-                             ID INT AUTO_INCREMENT PRIMARY KEY,
-                                                MeterID INT DEFAULT 98801006,
-                                                ReadAt DATETIME,
-                                                Global_active_power FLOAT,
-                                                Global_reactive_power FLOAT,
-                                                Voltage FLOAT,
-                                                Global_intensity FLOAT,
-                                                Sub_metering_1 FLOAT,
-                                                Sub_metering_2 FLOAT,
-                                                Sub_metering_3 FLOAT
-                                                )"""
-mycursor.execute(sql_global_active_power)
-
-
-month_data = """CREATE TABLE IF NOT EXISTS month_data (
+"""Table Structure
+month_data = \"""CREATE TABLE IF NOT EXISTS month_data (
                                       MeterID INT,
-                                      DateTime DATETIME,
+                                      usage_date DATETIME,
                                       KWH FLOAT,
                                       FOREIGN KEY (MeterID) REFERENCES Meter(MeterID)
-                                      )"""
-mycursor.execute(month_data)
+                                      )\"""
+                                      
 
-
-prediction_table = """CREATE TABLE IF NOT EXISTS prediction_table (
+NEW                                  
+prediction_table = \"""CREATE TABLE IF NOT EXISTS prediction_table (
                                       AreaID INT NOT NULL,
-                                      prediction_date DATETIME NOT NULL,
-                                      KWH FLOAT DEFAULT NULL,
                                       predicted_at DATETIME DEFAULT NULL,
+                                      minimum_KWH FLOAT DEFAULT NULL,
+                                      KWH FLOAT DEFAULT NULL,
+                                      maximum_KWH FLOAT DEFAULT NULL,
+                                      prediction_date DATETIME NOT NULL,
                                       FOREIGN KEY (AreaID) REFERENCES ServiceArea(AreaID)
-                                      )"""
-mycursor.execute(prediction_table)
+                                      )\"""
+                                      
+
+  """
+
+"""
+Need to check the insert query and send log info. to Admin when error occurs or too many attempts
+"""
 
 
-from dateutil import parser
-def insert_to_database_household(mydb, row):
-    insert_sql = ("INSERT INTO Household_power "
-                "(ReadAt,Global_active_power,Global_reactive_power,Voltage,Global_intensity,Sub_metering_1,Sub_metering_2,Sub_metering_3)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                )
-    row[0] = parser.parse(row[0])
-    cursor = mydb.cursor()
-    cursor.execute(insert_sql, row)
-
-    print(f'Insert to database {cursor.lastrowid}')
+def read_from_database(sql_query, con):
+    try:
+        read_data = pd.read_sql(sql=sql_query, con=con)
+        return read_data
+    except Exception as ex:
+        template_message = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template_message.format(type(ex).__name__, ex.args)
+        print(message)
 
 
-with open("testing_csv\household_data.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            insert_to_database_household(mydb, row)
-        line_count += 1
+def insert_to_database(data, mydb_sqlalchemy, tablename):
+    try:
+        print(data)
+        data.to_sql(con=mydb_sqlalchemy, name=tablename, if_exists='append')
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
 
 
-    mydb.commit()
-    print(f'Processed {line_count} lines')
-#End Global Active Power
-####################################
-
-
-
-def insert_to_database(mydb, row):
-    insert_sql = ("INSERT INTO MeterData "
-                "(MeterID, ReadAt, KWH, KW, KVA, KVAr, Ph1i, Ph2i, Ph3i, Ph1v, Ph2v, Ph3v, PF)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                )
-    row[1] = parser.parse(row[1])
-    cursor = mydb.cursor()
-    cursor.execute(insert_sql, row)
-
-    print(f'Insert to database {cursor.lastrowid}')
-
-
-
-
-with open("testing_csv\sample_data.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count != 0:
-            insert_to_database(mydb, row)
-        line_count += 1
-    mydb.commit()
-    print(f'Processed {line_count} lines')
+# Testing Purpose
+# try:
+#     # df = pd.read_sql_table("meter", database_connection_with_sqlalchemy(predicted_data))
+# mydb_sqlalchemy = database_connection_with_sqlalchemy()
+#     test = pd.read_sql("Select * from meter", mydb_sqlalchemy)
+#     print(test)
+#     # insert_to_database(predicted_data, mydb_sqlalchemy)
+# except Exception as e:
+#     print(e)
--- Prediction/resample_data.py
+++ Prediction/resample_data.py
@@ -6,7 +6,7 @@
 - This file will operate monthly at the end of month, eg. 31 January at 11:59PM
 - This will insert to table: month_data
 """
-import sys# testing
+import sys  # testing
 import time
 import connectToDatabase as connect
 import pandas as pd
@@ -19,9 +19,9 @@ YEAR = datetime.today().year
 
 # --------- Testing ---------
 MONTH = 11
-YEAR = 2017
+YEAR = 2007
 months = [x for x in range(1, 13)]
-years = [x for x in range(2006, 2011)]
+years = [x for x in range(2006, 2007)]
 print(months)
 print(years)
 # --------- End Testing ---------
@@ -41,7 +41,6 @@ This loop through all MeterId:
 """
 
 
-
 def get_meter_data(meter_id_in, meter_data_in, mydb_connection):
     meter_data_in.rename(columns={'ReadAt': 'usage_date'}, inplace=True)
     meter_data_in.set_index('usage_date', inplace=True)
@@ -50,38 +49,48 @@ def get_meter_data(meter_id_in, meter_data_in, mydb_connection):
     meter_data_in.interpolate(method='linear', inplace=True)
     meter_data_diff = meter_data_in.diff()
     data_in_month = meter_data_diff.resample('M').sum()
-    data_in_month['MeterID'] = int(meter_id_in)
-    print(data_in_month)# testing
-    connect.insert_to_database(data_in_month, mydb_connection, TABLE_NAME_DATA_MONTH)
-    print(data_in_month)# testing
+    data_in_month['MeterId'] = int(meter_id_in)
+    print(data_in_month)  # testing
+    connect.insert_to_database(
+        data_in_month, mydb_connection, TABLE_NAME_DATA_MONTH)
+    print(data_in_month)  # testing
 
 
 if __name__ == "__main__":
-    start_time = time.time()# testing
-    logging.basicConfig(filename="Error_at_resample_log.log", filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+    start_time = time.time()  # testing
+    logging.basicConfig(filename="Error_at_resample_log.log", filemode='w',
+                        format='%(asctime)s - %(levelname)s - %(message)s')
     # sys.exit()# testing
     mydb_sqlalchemy = connect.database_connection_with_sqlalchemy()
-    sql_query_get_meter_id = "SELECT MeterID FROM meter"
-    MeterID = connect.read_from_database(sql_query_get_meter_id, mydb_sqlalchemy)
-    meter_id_to_array = MeterID.values.reshape(-1, ).tolist()
+    sql_query_get_meter_id = "SELECT MeterId FROM Meter"
+    MeterID = connect.read_from_database(
+        sql_query_get_meter_id, mydb_sqlalchemy)
+    meter_id_to_array = MeterID.values.reshape(-1,).tolist()
+    print(meter_id_to_array)
     for meter_id in meter_id_to_array:
 
         try:
-            print(meter_id)# testing
+            print(meter_id)  # testing
             # sql_query_get_meter_data = "SELECT MAX(KWH) AS KWH, DATE_FORMAT(ReadAt, \"%Y-%m-%d 00:00:00\") AS ReadAt FROM meterdata WHERE MeterID={0} GROUP BY YEAR(ReadAt), MONTH(ReadAt), DAY(ReadAt)".format(meter_id)# testing with meterdata with given month and year
             # sql_query_get_meter_data = "SELECT MAX(Global_active_power) AS KWH, DATE_FORMAT(ReadAt, \"%Y-%m-%d 00:00:00\") AS ReadAt FROM household_power WHERE MeterID={0} GROUP BY YEAR(ReadAt), MONTH(ReadAt), DAY(ReadAt)".format(meter_id)# testing with household_power
-            sql_query_get_meter_data = "SELECT MAX(KWH) AS KWH, DATE_FORMAT(ReadAt, \"%Y-%m-%d 00:00:00\") AS ReadAt FROM meterdata WHERE MeterID={0} AND YEAR(ReadAt)={1} AND MONTH(ReadAt)={2} GROUP BY YEAR(ReadAt), MONTH(ReadAt), DAY(ReadAt)".format(meter_id, YEAR, MONTH)# Real Use
-            meter_data = connect.read_from_database(sql_query_get_meter_data, mydb_sqlalchemy)
+            sql_query_get_meter_data = "SELECT MAX(KWH) AS KWH, DATE_FORMAT(ReadAt, \"%Y-%m-%d 00:00:00\") AS ReadAt FROM MeterData WHERE MeterId={0} AND YEAR(ReadAt)={1} AND MONTH(ReadAt)={2} GROUP BY YEAR(ReadAt), MONTH(ReadAt), DAY(ReadAt)".format(
+                meter_id, YEAR, MONTH)  # Real Use
+            meter_data = connect.read_from_database(
+                sql_query_get_meter_data, mydb_sqlalchemy)
             print(meter_data)
             get_meter_data(meter_id, meter_data, mydb_sqlalchemy)
             # break# testing
-        except TypeError as err: # testing
-            print("An exception: " + type(err).__name__ + "\n" + str(err)) # testing
-            logging.error("Type {0} occurred. Arguments:\n{1!r}".format(type(err).__name__, err.args))# testing
-            continue # testing
+        except TypeError as err:  # testing
+            print("An exception: " + type(err).__name__ +
+                  "\n" + str(err))  # testing
+            logging.error("Type {0} occurred. Arguments:\n{1!r}".format(
+                type(err).__name__, err.args))  # testing
+            continue  # testing
         except Exception as err:
-            logging.error("Type {0} occurred. Arguments:\n{1!r}".format(type(err).__name__, err.args))
-            template_message = "An exception of type {0} occurred. Arguments:\n{1!r}"# testing
-            message = template_message.format(type(err).__name__, err.args)# testing
-            print(message)# testingz
-
+            logging.error("Type {0} occurred. Arguments:\n{1!r}".format(
+                type(err).__name__, err.args))
+            # testing
+            template_message = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template_message.format(
+                type(err).__name__, err.args)  # testing
+            print(message)  # testingz
