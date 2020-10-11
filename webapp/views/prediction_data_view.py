f7bad4a1a6a8667032c7fa4d6c3e1a4f3711fa03
Tola Veng <tolaswin@gmail.com>
2020-10-12 10:44:04 +1100

    distinct prediction date

--- webapp/views/prediction_data_view.py
+++ webapp/views/prediction_data_view.py
@@ -20,24 +20,35 @@ class PredictionView(ListView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        data = self.model.objects
+        data = self.model.objects.only('AreaId', 'predicted_at', 'minimum_KWH', 'KWH', 'maximum_KWH', 'prediction_date')
+        raw_sql = "SELECT DISTINCT MAX(predictionId) AS predictionId FROM prediction_table WHERE predictionId > 0"
 
         # Filters
         if self.requestUrl['areaId'] and self.requestUrl['areaId'] != 0:
             try:
                 area_id = self.requestUrl['areaId']
                 data = data.filter(AreaId=area_id)
+                raw_sql = raw_sql + " AND AreaId = " + area_id + ""
             except ValueError:
                 pass
 
         if self.requestUrl['year'] and self.requestUrl['year'] != '' and len(self.requestUrl['year']) == 4:
             try:
                 year = self.requestUrl['year']
-                data = data.filter(prediction_date__year__gte=year)
+                data = data.filter(prediction_date__year=year)
+                raw_sql = raw_sql + " AND YEAR(prediction_date) = " + year + ""
             except (ValueError, TypeError):
                 pass
 
-        return data.order_by('AreaId', 'prediction_date')
+        raw_sql = raw_sql + " GROUP BY AreaId, YEAR(prediction_date), MONTH(prediction_date)"
+        #print(raw_sql)
+
+        # Filter duplicate prediction data
+        raw = self.model.objects.raw(raw_sql)
+        ids = (obj.predictionId for obj in raw)
+        data = data.filter(predictionId__in=ids)
+
+        return data.order_by('AreaId', '-prediction_date').distinct()
 
     def get_context_data(self, **kwargs):
         context = super(PredictionView, self).get_context_data(**kwargs)
